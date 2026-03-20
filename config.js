838ba0bde7f7d3a4cf2a4bc2c7be29cbb43c6c3a
No <brajildesh11@gmail.com>
2026-03-20 12:51:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const JSONBIN_CONFIG = {
     apiKey: '$2a$10$BQtnaMjsytgKMhZyv7NMVOUizS8d58qKfjkEanNl1d10Jo1WsacUe',
-    binId:  '69bc3378c3097a1dd53de284'
+    binId:  '69bcf282c3097a1dd540dc3b'
 };
